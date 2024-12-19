8b535ab241b04cac9a6a424eb0f3b1b541e2c910
CutoffSquire917 <pushkin080109@gmail.com>
2024-12-20 00:52:15 +0100

    First task and i`m going sleep

--- hw-29(-1)/Source.cpp
+++ hw-29(-1)/Source.cpp
@@ -1,21 +1,66 @@
 #include <iostream>
 using namespace std;
 
+class Shape {
+public:
+	virtual double CalculateArea() const = 0;
+	virtual void Print() const = 0;
+};
+class Rectangle : public Shape {
+private:
+	double lenght;
+	double width;
+public:
+	Rectangle(const double l, const double w) : lenght(l), width(w) {}
+	virtual double CalculateArea() const override {
+		return lenght * width;
+	}
+	virtual void Print() const override {
+		cout << "Lenght : " << lenght << "\nWidth : " << width << "\nArea : " << CalculateArea() << endl;
+	}
+};
+class Circle : public Shape {
+private:
+	double radius;
+public:
+	Circle(const double r) : radius(r) {}
+	virtual double CalculateArea() const override {
+		return 3.14 * (radius * radius);
+	}
+	virtual void Print() const override {
+		cout << "Radius : " << radius << "\nArea : " << CalculateArea() << endl;
+	}
+};
+class Triangle : public Shape {
+private:
+	double height;
+	double base;
+public:
+	Triangle(const double h, const double b) : height(h), base(b) {}
+	virtual double CalculateArea() const override {
+		return (height * base) / 2;
+	}
+	virtual void Print() const override {
+		cout << "Height : " << height << "\nBase : " << base << "\nArea : " << CalculateArea() << endl;
+	}
+};
+
+
+
 int main()
 {
-	//Завдання 1: Фігури на площині
-	//Створіть ієрархію класів для геометричних фігур:
-	//Базовий клас Shape:
-	//Містить віртуальний метод CalculateArea() для обчислення площі.
-	//Містить віртуальний метод Print() для виведення інформації про фігуру.
-	
-	//Класи-нащадки:
-	//Rectangle: має довжину і ширину, реалізує методи CalculateArea() і Print().
-	//Circle: має радіус, реалізує методи CalculateArea() і Print().
-	//Triangle: має основу і висоту, реалізує методи CalculateArea() і Print().
-	
-	//У main створіть масив з вказівників на базовий клас Shape. Додайте до нього кілька об'єктів
-	//різних фігур. Викличте методи CalculateArea() та Print() для всіх об'єктів через базовий клас.
+	Shape* shapes[] = {
+		new Rectangle(5.0, 3.0),
+		new Circle(2.5),
+		new Triangle(4.0, 2.0)	
+	};
+	for (Shape* shape : shapes) {
+		shape->Print();
+		cout << endl;
+		delete shape;
+	}
+
+
 
 
 	//Завдання 2: Тварини у зоопарку
